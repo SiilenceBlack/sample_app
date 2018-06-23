a183457451c4cd4be925d380bf65e8315ae66515
SiilenceBlack <N.Kischke@web.de>
2018-06-23 20:31:31 +0200

    created products table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :products
   get 'simple_pages/index'
   root 'simple_pages#index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
+  get 'products/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
